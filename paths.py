8c2e3eec433006f9eb4badff79724db4cdc36a75
steindorfftk <147119154+steindorfftk@users.noreply.github.com>
2024-02-07 11:18:52 -0300

    Delete paths.py

--- paths.py
+++
@@ -1,8 +0,0 @@
-#Paths
-if organism == 'Mus musculus':
-	refGenomePath = 'temporary/bowtie2/refGenomes/mm10.fasta'
-	annotationPath = 'temporary/feature_counts/data/mm10_RefSeq_exon.txt'
-elif organism == 'Homo sapiens':
-	refGenomePath = 'temporary/bowtie2/refGenomes/GRCh38_noalt_as'
-	annotationPath = 'temporary/feature_counts/data/hg38_RefSeq_exon.txt'
-
